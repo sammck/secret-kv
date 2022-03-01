0b261cfbd7f25ca9bf44bf4007d0e6d37c81ffc1
Sam McKelvie <sammck@gmail.com>
2022-03-01 00:25:38 -0800

    basic set command, beginning get

--- secret_kv/__main__.py
+++ secret_kv/__main__.py
@@ -8,11 +8,15 @@
 """Command-line interface for secret_kv package"""
 
 
-import argparse
-from typing import Optional, Sequence, List
+import base64
+from typing import Optional, Sequence, List, Union
 
 import os
 import sys
+import argparse
+import argcomplete
+import json
+from base64 import b64encode, b64decode
 
 # NOTE: this module runs with -m; do not use relative imports
 from secret_kv import (
@@ -22,8 +26,10 @@ from secret_kv import (
 from secret_kv import (
     KvStoreConfig,
     KvStore,
+    KvValue,
     locate_kv_store_config_file,
     load_kv_store_config,
+    delete_kv_store,
   )
 
 class CmdExitError(RuntimeError):
@@ -35,6 +41,8 @@ class CmdExitError(RuntimeError):
 
 class CommandHandler:
   _argv: Optional[Sequence[str]]
+  _parser: argparse.ArgumentParser
+  _args: argparse.Namespace
   _cwd: str
   _config_file: Optional[str] = None
   _store_config: Optional[KvStoreConfig] = None
@@ -69,22 +77,153 @@ class CommandHandler:
     print("A command is required", file=sys.stderr)
     return 1
 
-  def cmd_clear_database(self, args: argparse.Namespace) -> int:
+  def cmd_clear_database(self) -> int:
     self._erase_db = True
     kv = self.get_kv_store()
     print(f"{kv} successfully cleared.", file=sys.stderr)
     return 0
 
-  def cmd_test(self, args: argparse.Namespace) -> int:
+  def cmd_delete_store(self) -> int:
+    config_file = self.get_config_file()
+    store_desc = delete_kv_store(config_file, scan_parent_dirs=False)
+    print(f"secret-cv store with config at '{store_desc}' successfully deleted.", file=sys.stderr)
     return 0
 
-  def cmd_create(self, args: argparse.Namespace) -> int:
+  def cmd_test(self) -> int:
+    return 0
+
+  def cmd_create(self) -> int:
+    args = self._args
     parent_dir = self.abspath(args.parent_dir)
     passphrase: Optional[str] = args.passphrase
 
     db = create_kv_store(parent_dir, passphrase=passphrase)
 
     print(f"Successfully created .secret-kv store under {parent_dir}", file=sys.stderr)
+    return 0
+
+  def cmd_get(self) -> int:
+    #
+    """
+    parser_set.add_argument('key',
+                        help='The key name for which a value is being set')
+    parser_set.add_argument('value', nargs='?', default=None,
+                        help='The value to assign to the key. By default, interpreted as a string value. See options for interpretaton.')
+    parser_set.add_argument('-t', '--type', dest='value_type', default=None, choices= [ 'str', 'int', 'float', 'bool', 'json', 'base64', 'binary' ],
+                        help='Specify how the provided value is interpreted')
+    parser_set.add_argument('--json', dest="vtype_json", action='store_true', default=False,
+                        help='short for --type=json')
+    parser_set.add_argument('--int', dest="vtype_int", action='store_true', default=False,
+                        help='short for --type=int')
+    parser_set.add_argument('--stdin', dest="use_stdin", action='store_true', default=False,
+                        help='Read the value from stdin instead of the commandline')
+    parser_set.add_argument('-i', '--input', dest="input_file", default=None,
+                        help='Read the value from the specified file instead of the commandline')
+
+    """
+    args = self._args
+    key: str = args.key
+    store = self.get_kv_store()
+    kv = store.get_value(key)
+    if kv is None:
+      raise KeyError(f"secret-kv: get: key \"{key}\" does not exist")
+    print(json.dumps(kv.json_data, indent=2, sort_keys=True))
+
+  def cmd_set(self) -> int:
+    #
+    """
+    parser_set.add_argument('key',
+                        help='The key name for which a value is being set')
+    parser_set.add_argument('value', nargs='?', default=None,
+                        help='The value to assign to the key. By default, interpreted as a string value. See options for interpretaton.')
+    parser_set.add_argument('-t', '--type', dest='value_type', default=None, choices= [ 'str', 'int', 'float', 'bool', 'json', 'base64', 'binary' ],
+                        help='Specify how the provided value is interpreted')
+    parser_set.add_argument('--json', dest="vtype_json", action='store_true', default=False,
+                        help='short for --type=json')
+    parser_set.add_argument('--int', dest="vtype_int", action='store_true', default=False,
+                        help='short for --type=int')
+    parser_set.add_argument('--stdin', dest="use_stdin", action='store_true', default=False,
+                        help='Read the value from stdin instead of the commandline')
+    parser_set.add_argument('-i', '--input', dest="input_file", default=None,
+                        help='Read the value from the specified file instead of the commandline')
+
+    """
+    args = self._args
+    key: str = args.key
+    value_s: Optional[str] = args.value
+    value_type_s: Optional[str] = args.value_type
+    if args.vtype_json:
+      if value_type_s is None:
+        value_type_s = 'json'
+      elif value_type_s != 'json':
+        raise ValueError(f"secret-kv: set: Conflicting value types {value_type_s} and json")
+    if args.vtype_int:
+      if value_type_s is None:
+        value_type_s = 'int'
+      elif value_type_s != 'int':
+        raise ValueError(f"secret-kv: set: Conflicting value types {value_type_s} and int")
+    if value_type_s is None:
+      value_type_s = 'str'
+    use_stdin: bool = args.use_stdin
+    input_file: Optional[str] = args.input_file
+    if use_stdin:
+      if input_file is None:
+        input_file = '/dev/stdin'
+      else:
+        raise ValueError(f"secret-kv: set: Conflicting value input sources, --stdin and \"{input_file}\"")
+    value: Union[str, bytes]
+    if input_file is None:
+      if value_s is None:
+        raise ValueError("secret-kv: set: One of <value>, --stdin, or --input <filename> must be provided.")
+      if value_type_s == 'binary':
+        value = value_s.encode('utf-8')
+      else:
+        value = value_s
+    else:
+      if not value_s is None:
+        raise ValueError("secret-kv: set: <value> must be omitted if -i, --input, or --stdin is provided.")
+      mode = 'b' if value_type_s == 'binary' else 't'
+      with open(input_file, mode) as f:
+        value = f.readall()
+
+    if value_type_s == 'base64':
+      try:
+        value = b64decode(value, validate=True)
+      except Exception as ex:
+        raise ValueError(f"secret-kv: set: Invalid base-64 encoded string: {ex}") from ex
+      value_type_s = 'binary'
+
+    if value_type_s == 'int':
+      try:
+        value = int(value)
+      except ValueError as ex:
+        raise ValueError(f"secret-kv: set: Invalid integer literal") from ex
+    elif value_type_s == 'float':
+      try:
+        value = float(value)
+      except ValueError as ex:
+        raise ValueError(f"secret-kv: set: Invalid float literal") from ex
+    elif value_type_s == 'bool':
+      value = value.lower()
+      if value in [ 'true', 't', 'yes', 'y', '1' ]:
+        value = 'true'
+      elif value in [ 'false', 'f', 'no', 'n', '0' ]:
+        value = 'false'
+      else:
+        raise ValueError(f"secret-kv: set: Invalid boolean literal: '{value}'")
+    elif value_type_s == 'json':
+      try:
+        value = json.loads(value)
+      except json.JSONDecodeError as ex:
+        raise ValueError(f"secret-kv: set: Invalid JSON text: {ex}") from ex
+
+    elif value_type_s in [ 'str', 'binary' ]:
+      pass
+
+    kv = KvValue(value)
+    store = self.get_kv_store()
+    store.set_value(key, kv)
+
     return 0
 
   def run(self) -> int:
@@ -101,6 +240,7 @@ class CommandHandler:
     import argparse
 
     parser = argparse.ArgumentParser(description="Access a secret key/value database.")
+    self._parser = parser
     parser.add_argument('--version', action='store_true', default=False,
                         help='Display version')
     parser.add_argument('-C', '--cwd', default='.',
@@ -129,13 +269,42 @@ class CommandHandler:
     parser_clear_database = subparsers.add_parser('clear-database', description="Erases all content in the database without deleting it")
     parser_clear_database.set_defaults(func=self.cmd_clear_database)
 
+    parser_delete_store = subparsers.add_parser('delete-store', description="Deletes the secret-kv store, including database, config, and .secret-kv directory")
+    parser_delete_store.set_defaults(func=self.cmd_delete_store)
+
+    parser_set = subparsers.add_parser('set', description="Set the value associated with a key")
+    parser_set.add_argument('key',
+                        help='The key name for which a value is being set')
+    parser_set.add_argument('value', nargs='?', default=None,
+                        help='The value to assign to the key. By default, interpreted as a string value. See options for interpretaton.')
+    parser_set.add_argument('-t', '--type', dest='value_type', default=None, choices= [ 'str', 'int', 'float', 'bool', 'json', 'base64', 'binary' ],
+                        help='Specify how the provided value is interpreted')
+    parser_set.add_argument('--json', dest="vtype_json", action='store_true', default=False,
+                        help='short for --type=json')
+    parser_set.add_argument('--int', dest="vtype_int", action='store_true', default=False,
+                        help='short for --type=int')
+    parser_set.add_argument('--stdin', dest="use_stdin", action='store_true', default=False,
+                        help='Read the value from stdin instead of the commandline')
+    parser_set.add_argument('-i', '--input', dest="input_file", default=None,
+                        help='Read the value from the specified file instead of the commandline')
+    parser_set.set_defaults(func=self.cmd_set)
+
+    parser_get = subparsers.add_parser('get', description="Get the value associated with a key")
+    parser_get.add_argument('key',
+                        help='The key name for which the value is being fetched')
+    parser_get.add_argument('-o', '--output', dest="output_file", default=None,
+                        help='Write the value to the specified file instead of stdout')
+    parser_get.set_defaults(func=self.cmd_get)
+
+    argcomplete.autocomplete(parser)
     args = parser.parse_args(self._argv)
+    self._args = args
     self._cwd = os.path.abspath(os.path.expanduser(args.cwd))
     self._passphrase = args.passphrase
     config_file: Optional[str] = args.config
     if not config_file is None:
       self._config_file = self.abspath(config_file)
-    rc = args.func(args)
+    rc = args.func()
 
     return rc
 
