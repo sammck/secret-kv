a7c129c62ff797766303ef8cc1716c58a48e7a33
semantic-release <semantic-release>
2022-03-04 10:07:38 -0800

    0.4.5
    Automatically generated by python-semantic-release

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.4.5 (2022-03-04)
+
--- secret_kv/version.py
+++ secret_kv/version.py
@@ -11,7 +11,7 @@
 
 
 # The following line is automatically updated with "semantic-release version"
-__version__ =  "0.4.4"
+__version__ =  "0.4.5"
 
 
 __all__ = [ __version__ ]
