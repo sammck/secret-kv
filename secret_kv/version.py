c65f681a2558e754b5128faac28a3e6d0ea7e18a
Sam McKelvie <sammck@gmail.com>
2022-03-04 09:57:55 -0800

    semantic releases

--- secret_kv/version.py
+++ secret_kv/version.py
@@ -6,10 +6,12 @@
 """Package secret_kv provides encrypted rich key/value storage for an application or project
 """
 
-import importlib.metadata as _metadata
+# import importlib.metadata as _metadata
+# __version = _metadata.version(__package__.replace('_','-')) #  e.g., '0.1.0'
 
+
+# The following line is automatically updated with "semantic-release version"
 __version__ =  "0.4.3"
 
-# __version = _metadata.version(__package__.replace('_','-')) #  e.g., '0.1.0'
 
 __all__ = [ __version__ ]
